4e7926fdbd80e9905e0aa52894acafb46be9bd57
caoyao <caoyao@testbird.com>
2019-07-12 18:27:10 +0800

    fix config bug

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ pip3 install -r requirements.txt
 #python src/manage.py collectstatic --noinput --settings=settings.prod
 #python src/manage.py migrate --settings=settings.prod
 #
-#supervisorctl restart all
+supervisorctl restart all
 #sudo service nginx reload
