adb90fc36993d5ffc6444a57d83d074c707fe392
caoyao <caoyao@testbird.com>
2019-07-12 16:01:11 +0800

    requirements

--- app.py
+++ app.py
@@ -22,6 +22,10 @@ def gettoken():
                         }})
     return res.text
 
+@app.route('/')
+def index():
+    return "HELLO"
+
 
 if __name__ == '__main__':
     app.run(debug=False, host="0.0.0.0", port=10001)
