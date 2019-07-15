c4901625a362e0f66c1a78167cb95e636cf98e6c
caoyao <caoyao@testbird.com>
2019-07-15 21:24:43 +0800

    3

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,5 @@
 git pull
 source /flasktest/venv/bin/activate
 pip3 install -r requirements.txt
-#python src/manage.py collectstatic --noinput --settings=settings.prod
-#python src/manage.py migrate --settings=settings.prod
-#
 supervisorctl restart all
 sudo service nginx reload
