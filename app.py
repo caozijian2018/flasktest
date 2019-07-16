97fa63b3f68858da1c8a52f4801c2f7d72f6907c
caoyao <caoyao@testbird.com>
2019-07-16 10:43:21 +0800

    4

--- app.py
+++ app.py
@@ -10,28 +10,26 @@ logging_format = logging.Formatter(
     '%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(lineno)s - %(message)s')
 handler.setFormatter(logging_format)
 app.logger.addHandler(handler)
-
-print(999)
 @app.route('/')
 def static_views():
-    # logging.info("laoziziziz")
-    app.logger.info("my first logging")
-    print(123123444123)
     return render_template('ii.html')
 
 
 @app.route('/gettoken', methods=['GET', 'POST'])
 def gettoken():
+    # if request.META.get("HTTP_X_FORWARDED_FOR", None):
+    #     ip = request.META['HTTP_X_FORWARDED_FOR']
+    # else:
+    #     ip = request.META.get("REMOTE_ADDR", "")
     ip = request.remote_addr
-    data = request.json
+    data = request.values
     token = data.get("token", "")
     vaify_url = "https://www.google.com/recaptcha/api/siteverify"
     data = {"secret": "6LefJa0UAAAAANhH9-OH7A-o1BoWNUQMZ0x82L7P", "response": token,
                               "remoteip": ip}
 
     app.logger.info(str(data))
-    res = requests.post(vaify_url,
-                        data=data)
+    res = requests.post(url=vaify_url, data=data)
     app.logger.info(str(res))
     return json.dumps({"code": 1, "msg": "success"})
 
