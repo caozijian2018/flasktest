77a90be5d0f8b12170efec116cf9e00596f0da03
caoyao <caoyao@testbird.com>
2019-07-16 14:37:24 +0800

    4

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git pull
-source /flasktest/venv/bin/activate
+source /data/flasktest/venv/bin/activate
 pip3 install -r requirements.txt
 supervisorctl restart all
 sudo service nginx reload
