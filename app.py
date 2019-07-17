8875d937a3760ac8f60113d861e63beb66f5a63f
caoyao <caoyao@testbird.com>
2019-07-17 18:34:45 +0800

    mongo

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, request, render_template, make_response, jsonify
 import requests
 import json
 import logging
-import pymongo
+# import pymongo
 app = Flask(__name__)
-MONGO_CLI = pymongo.MongoClient(host='127.0.0.1', port='27017')
+# MONGO_CLI = pymongo.MongoClient(host='127.0.0.1', port='27017')
 logging.basicConfig(filename="./logs/test.log", filemode="w", format="%(asctime)s %(name)s:%(levelname)s:%(message)s", datefmt="%d-%M-%Y %H:%M:%S", level=logging.DEBUG)
 # 设置log
 @app.route('/')
@@ -41,6 +41,42 @@ def getantitoken():
     }}
     res = requests.post(url=url, data=data).json()
     logging.info("ANTItoken{}".format(str(res)))
+
+
+@app.route('/get2captchaid', methods=['POST'])
+def get2captchaid():
+    data = request.values
+    min_score = data.get('min_score', '0.3')
+    pageurl = data.get('pageurl', '')
+
+    url = "http://2captcha.com/in.php"
+    data = {"key": "46f989bbda665e16b138119bd15c140e",
+            "method": "userrecaptcha",
+            "googlekey": "6LefJa0UAAAAAOB06AdDI0L7krmqGle-gdob4WDw",
+            "pageurl": "http://static.humorboom.com/testv3/index.html",
+            "version": "v3",
+            "action": 'verify',
+            "min_score": min_score
+            }
+    res = requests.post(url=url, data=data).text.split("|")[1]
+    logging.info("ANTItoken{}".format(str(res)))
+    return jsonify(res)
+
+@app.route('/get2captcharesult', methods=['POST'])
+def get2captcharesult():
+    data = request.values
+    taskid = data.get('taskid', '')
+    url = "http://2captcha.com/res.php"
+    data = {
+        "key": "46f989bbda665e16b138119bd15c140e",
+        "action": 'get',
+        "taskinfo": 1,
+        "json": 1,
+        "id": taskid
+    }
+    res = requests.get(url=url, data=data).json()
+    logging.info("ANTItoken{}".format(str(res)))
+    return jsonify(res)
 
 
 if __name__ == '__main__':
