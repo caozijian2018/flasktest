4d350b59addaef370cd2b8718a8c5e324a7f41b2
caoyao <caoyao@testbird.com>
2019-07-17 16:22:04 +0800

    mongo

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, request, render_template, make_response, jsonify
 import requests
 import json
 import logging
+import pymongo
 app = Flask(__name__)
+MONGO_CLI = pymongo.MongoClient(host='127.0.0.1', port='27017')
 logging.basicConfig(filename="./logs/test.log", filemode="w", format="%(asctime)s %(name)s:%(levelname)s:%(message)s", datefmt="%d-%M-%Y %H:%M:%S", level=logging.DEBUG)
 # 设置log
 @app.route('/')
@@ -25,6 +27,21 @@ def gettoken():
     logging.info("返回.{}".format(str(res)))
 
     return jsonify(res)
+
+
+@app.route('/getantitoken', methods=['POST'])
+def getantitoken():
+    url = "http://api.anti-captcha.com/createTask"
+    data = {"clientKey": "2430681487daa93cd7a0490504f54af5", "task": {
+        "type": "RecaptchaV3TaskProxyless",
+        "websiteURL": "http://static.humorboom.com/testv3/index.html",
+        "websiteKey": "6LefJa0UAAAAAOB06AdDI0L7krmqGle-gdob4WDw",
+        "minScore": 0.3,
+        "pageAction": "homepage"
+    }}
+    res = requests.post(url=url, data=data).json()
+    logging.info("ANTItoken{}".format(str(res)))
+
 
 if __name__ == '__main__':
     app.run()
